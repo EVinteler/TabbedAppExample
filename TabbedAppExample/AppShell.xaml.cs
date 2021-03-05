a13a2bffe34c1d2900d5f898738ee2c288fced61
EVinteler <ericavinteler@yahoo.com>
2021-03-05 17:56:53 +0200

    Add project files.

---
+++ TabbedAppExample/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TabbedAppExample.ViewModels;
+using TabbedAppExample.Views;
+using Xamarin.Forms;
+
+namespace TabbedAppExample
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
